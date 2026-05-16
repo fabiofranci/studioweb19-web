ef1e71ed6b7d1331110ab40a22cbaa10619fa859
Fabio Franci <fabio.franci@gmail.com>
2026-05-16 18:21:50 +0200

    feat: add dynamic blog and project routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+
+  site: 'https://studioweb19.it',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
+
 });
