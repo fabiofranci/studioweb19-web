b3aaa513935e9df72d0874993f5ee521df327218
Fabio Franci <fabio.franci@gmail.com>
2026-05-16 18:28:35 +0200

    feat: add seo and metadata foundation

---
+++ src/lib/site.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+    title: 'StudioWeb19',
+    description:
+        'Sviluppo software gestionali, piattaforme cloud e sistemi verticali in Laravel.',
+
+    url: 'https://studioweb19.it',
+
+    author: 'Fabio Franci',
+
+    links: {
+        github: 'https://github.com/fabiofranci',
+    },
+};
